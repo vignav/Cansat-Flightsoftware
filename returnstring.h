5b53eb7e96b58a5b524162d98be9eeac415cfa76
Umang_Tekriwal <umangtekriwal1@gmail.com>
2023-03-07 01:40:56 +0530

    Modified printFloat(), printInt() and created two support functions to add zeros to suffix and prefix

--- returnstring.h
+++ returnstring.h
@@ -1,55 +1,126 @@
 #include <iostream>
 #include <string.h>
 #include <iomanip>
-#include <sstream>
+//#include <sstream>
 using namespace std;
 
 
 
 
-string printFloat(double str, int len,bool valid)
-{
-  string s="";
-  string stri="";
+// string printFloat(double str, int len,bool valid)
+// {
+//   string s="";
+//   string stri="";
   
-  if (valid)
-  {
-    string dot = ".";
-    stringstream s;
-    s<<str;
-    stri = s.str();
-    size_t a = stri.find(dot);
+//   if (valid)
+//   {
+//     string dot = ".";
+//     stringstream s;
+//     s<<str;
+//     stri = s.str();
+//     size_t a = stri.find(dot);
     
-    for (int i=a-1; i<len; ++i)
-      stri+=("'");
-  }
-  else
-  {
+//     for (int i=a-1; i<len; ++i)
+//       stri+=("'");
+//   }
+//   else
+//   {
     
-    for (int i=0; i<len; i++)
-      stri+="*";
-  }
-  return stri;
-}
+//     for (int i=0; i<len; i++)
+//       stri+="*";
+//   }
+//   return stri;
+// }
 
-string printInt(unsigned long val, int len, bool valid)
+string addZeroSuffix(string st, int n)
 {
-  string str="";
+  for(int i=1;i<=n;i++)
+    st = st + "0";
+  
+  return st;
+}
+
+string addZeroToPrefix(string st, int n)
+{
+  for(int i=1;i<=n;i++)
+    st = "0" + st;
+
+  return st;
+}
+
+string printFloat(double str, int len, int lenAfterDecimal, bool valid)
+{
+  string finalStri;
   if (valid)
   {
-    str = to_string(val);
-    for (int i=str.size(); i<len; ++i)
-      str+=(' ');
+    string s = to_string(str);
+    size_t indexOfPt = s.find(".");
+
+    if((len)-(indexOfPt+1) >= lenAfterDecimal)
+      finalStri = s.substr(0, (indexOfPt+1+lenAfterDecimal));
+    else
+    {
+      int zeroToSuffix = lenAfterDecimal - (len - (indexOfPt+1));
+      finalStri = addZeroSuffix(s, zeroToSuffix);
+    }
+
+      size_t lenBeforeDecimal = len - (lenAfterDecimal + 1);
+
+      if(lenBeforeDecimal >= indexOfPt)
+      {
+        int zeroToPrefix = lenBeforeDecimal - indexOfPt;
+        finalStri = addZeroToPrefix(finalStri, zeroToPrefix);
+      }
+      else
+        return "INVALID";
+
+      return finalStri;
+  }
+  else
+  {
+    
+    for (int i=0; i<len; i++)
+      finalStri+="*";
+  }
+  return finalStri;
+}
+
+// string printInt(unsigned long val, int len, bool valid)
+// {
+//   string str="";
+//   if (valid)
+//   {
+//     str = to_string(val);
+//     for (int i=str.size(); i<len; ++i)
+//       str+=(' ');
+//   }
+
+//   else
+//   {
+//     for (int i=0; i<len; i++)
+//       str+="*";
+//   }
+//   return(str);
+// }
+ 
+ string printInt(unsigned long val, int len, bool valid)
+{
+  string finalStr="";
+  if (valid)
+  {
+    string st = to_string(val);
+    size_t zeroAtPrefix = len - (st.length());
+    finalStr = addZeroToPrefix(st, zeroAtPrefix);
+    return finalStr;
   }
 
   else
   {
     for (int i=0; i<len; i++)
-      str+="*";
+      finalStr+="*";
   }
-  return(str);
+  return(finalStr);
 }
- 
 
 
 
