fdf12f8b4d299e97c9942d532ce1c0fe9ec58e59
Vignav <vignav.ram@gmail.com>
2023-03-06 20:15:00 +0530

    gps sensor one funciton created

--- sensors/gpssensor.h
+++ sensors/gpssensor.h
@@ -1,10 +1,37 @@
 #include <TinyGPSPlus.h>
+#include "returnstring.h"
 
 TinyGPSPlus gps;
-#define ss Serial1
+#define gpsSerial Serial1
+#define gpsBaud 9600
 
-static const uint32_t GPSBaud = 9600;
+void gpsSetup(){
+  gpsSerial.begin(GPSBaud);
+}
 
+bool gpsSatsValid(){
+ return (gps.satellites.isValid());
+}
+
+float gpsAlt(){
+  return ( gps.altitude.meters() );
+}
+/*
+void gpsReading(){
+  printInt(gps.satellites.value(), , 5);
+  printFloat(gps.location.lat(), gps.location.isValid(), 11, 6);
+  printFloat(gps.location.lng(), gps.location.isValid(), 12, 6);
+  gps.date; gps.time;
+  gps.altitude.isValid();
+}
+
+void gpsTime(){
+  gps.date.isValid();
+  gps.time.isValid();
+  gps.date.month(); gps.date.day(); gps.date.year();
+  gps.time.hour(); gps.time.minute(); gps.time.second();
+}
+*/
 // This custom version of delay() ensures that the gps object
 // is being "fed".
 static void smartDelay(unsigned long ms)
@@ -12,7 +39,7 @@ static void smartDelay(unsigned long ms)
   unsigned long start = millis();
   do 
   {
-    while (ss.available())
-      gps.encode(ss.read());
+    while (gpsSerial.available())
+      gps.encode(gpsSerial.read());
   } while (millis() - start < ms);
 }
