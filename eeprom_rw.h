7233d34053c77f4997170d78af50837f08d76a95
Vignav <vignav.ram@gmail.com>
2023-03-07 23:18:17 +0530

    eeprom functions names changed

--- eeprom_rw.h
+++ eeprom_rw.h
@@ -1,25 +1,25 @@
 #include <EEPROM.h>
 
-float readFloat( int n ){
+float EEreadFloat( int n ){
       unsigned int eeAddress = ( sizeof(float)>sizeof(int) ? sizeof(float) : sizeof(int)) *n;
       float data = 0.00f;  
       EEPROM.get( eeAddress , data );
       return data;
 }
 
-int readInt( int n ){
+int EEreadInt( int n ){
       unsigned int eeAddress = ( sizeof(float)>sizeof(int) ? sizeof(float) : sizeof(int)) *n;
       int data = 0;  
       EEPROM.get( eeAddress , data );
       return data;
 }
 
-void writeFloat( float data , int n ){
+void EEwriteFloat( float data , int n ){
       unsigned int eeAddress = ( sizeof(float)>sizeof(int) ? sizeof(float) : sizeof(int)) *n;
       EEPROM.put( eeAddress , data );
 }
 
-void writeInt( int data , int n ){
+void EEwriteInt( int data , int n ){
       unsigned int eeAddress = ( sizeof(float)>sizeof(int) ? sizeof(float) : sizeof(int)) *n;
       EEPROM.put( eeAddress , data );
 }
