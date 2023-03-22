54e9d25823d50ac212ad422acb08c50e5f765972
Vignav <vignav.ram@gmail.com>
2023-03-22 22:31:24 +0530

    few variables added and changes made

--- Cansat-Flightsoftware.ino
+++ Cansat-Flightsoftware.ino
@@ -14,10 +14,13 @@ bool HS_deployed = false;
 bool PC_deployed = false;
 bool MAST_raised = false;
 
-float voltage;
 
-float zero_alt_calib;
+float zero_alt_calib = 0;
 bool telemetry = true;
+bool tilt_calibration = false ;
+bool simulation_enabled = false;
+
+float voltage = 0;
 
 float temprature = 0 , altitude = 0 , pressure = 0 ; 
 bool bmpValid = false ;
@@ -53,6 +56,13 @@ void loop() {
   // put your main code here, to run repeatedly:
   switch(currentState){
     case IDLE:
+      if(tilt_calibration){
+        int tilt_cal_status = bnoCalibration();
+        sendDataTelemetry(String("Tilt Calibration: ")String(tilt_cal_status)+String("|"));
+        if ( !tilt_cal_status ){
+          tilt_calibration = false;
+        }
+      }
       break;
     case LAUNCH_WAIT:
       // check if cansat has started accending if yes change state
@@ -107,6 +117,9 @@ void repetitive_Task( ){
     String CMD_ECHO="";
 
     //get packet 
+    if( packetAvailable() ){
+      String packetRecieved = recieveDataTelemetry();
+    }
     //process it 
 
     
