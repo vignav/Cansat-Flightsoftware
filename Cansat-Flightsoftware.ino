1f570e9b91fbd10b9b69d4908f00b709a456c91f
Vignav <vignav.ram@gmail.com>
2023-04-17 18:25:54 +0530

    Tilt Calibration Command can be used and works well

--- Cansat-Flightsoftware.ino
+++ Cansat-Flightsoftware.ino
@@ -3,7 +3,7 @@ enum states {
   LAUNCH_WAIT ,
   ASCENT ,
   DECENT ,
-  PAYLOAD_SEPARATED , 
+  PAYLOAD_SEPARATED ,
   PARACHUTE_DEPLOYED ,
   LANDED
 };
@@ -14,7 +14,7 @@ enum modes {
 
 int currentState = IDLE ;
 int currentMode = FLIGHT ;
-int packet_count=0;
+int packet_count = 0;
 bool HS_deployed = false;
 bool PC_deployed = false;
 bool MAST_raised = false;
@@ -27,7 +27,7 @@ bool simulation_enabled = false;
 
 float voltage = 0;
 
-float temprature = 0 , altitude = 0 , pressure = 0 ; 
+float temprature = 0 , altitude = 0 , pressure = 0 ;
 bool bmpValid = false ;
 
 float xAngle = 0 , yAngle = 0 , zAngle = 0 , acceleration = 0 ;
@@ -37,7 +37,7 @@ float noSats = 0 , lat = 0 , lng = 0 , gpsAltitude = 0 ;
 bool satsValid = false, locValid = false, altValid = false;
 
 int gpsSecond = 0 , gpsMinute = 0 , gpsHour = 0  , gpsDay = 0 , gpsMonth = 0, gpsYear = 0 ;
-bool timeValid =false , dateValid =false ;
+bool timeValid = false , dateValid = false ;
 
 #include "./sensors/battery.h"
 #include "./sensors/gpssensor.h"
@@ -52,7 +52,7 @@ bool timeValid =false , dateValid =false ;
 #include "smartDelay.h"
 #include "./sensors/bnosensor.h"
 
-void setup(){
+void setup() {
   // put your setup code here, to run once:
   // Go to EEPROM and get values of state, mode, packet_count and zero_alt_calibration
   currentState = EEreadInt(1);
@@ -70,47 +70,47 @@ void setup(){
 
 void loop() {
   // put your main code here, to run repeatedly:
-  switch(currentState){
+  switch (currentState) {
     case IDLE:
-      if(tilt_calibration){
+      if (tilt_calibration) {
         int tilt_cal_status = bnoCalibration();
-        sendDataTelemetry(String("Tilt Calibration: ")+String(tilt_cal_status)+String("|"));
-        if ( !tilt_cal_status ){
+        sendDataTelemetry(String("Tilt Calibration: ") + String(tilt_cal_status) + String("|"));
+        if ( !tilt_cal_status ) {
           tilt_calibration = false;
         }
       }
       break;
     case LAUNCH_WAIT:
       // check if cansat has started accending if yes change state
-      if ( movingUp() ){
+      if ( movingUp() ) {
         currentState = ASCENT;
       }
       break;
     case ASCENT:
       // check if cansat has stopped accent and started going downwards ( decreasing altitude)
-      if ( movingDown() ){
+      if ( movingDown() ) {
         currentState = DECENT;
       }
       break;
     case DECENT:
-      // Check if altitude is less than 500m if yes change state to payload_separated 
-      if ( checkAlt(500) ){
+      // Check if altitude is less than 500m if yes change state to payload_separated
+      if ( checkAlt(500) ) {
         currentState = PAYLOAD_SEPARATED;
       }
       break;
     case PAYLOAD_SEPARATED:
-      //Check if altitude is 200m then change state and relase parachute 
+      //Check if altitude is 200m then change state and relase parachute
       deployProbe();
-      deployHeatSheild();//probably release this after little height 
-      if ( checkAlt(200) ){
+      deployHeatSheild();//probably release this after little height
+      if ( checkAlt(200) ) {
         currentState = PARACHUTE_DEPLOYED;
       }
       break;
     case PARACHUTE_DEPLOYED:
       //deploy parachute function
       deployParachute();
-       //If there is no movement then move to landed state
-      if( notMoving() ){
+      //If there is no movement then move to landed state
+      if ( notMoving() ) {
         currentState = LANDED;
       }
       break;
@@ -125,51 +125,51 @@ void loop() {
 
 
   }
-  smartDelay(1000); 
+  smartDelay(1000);
   repetitive_Task();
 }
 
 
-void repetitive_Task( ){
-    packet_count ++;
-    
-    // Read Sensor Data
-    //GPS data
-    gpsGetTime( &gpsSecond , &gpsMinute, &gpsHour , &gpsDay, &gpsMonth , &gpsYear , &dateValid , &timeValid);        
-    gpsReading(&noSats , &lat , &lat , &gpsAltitude , &satsValid, &locValid  , &altValid );
-    //BNO data
-    bnoGetValues();
-    readVoltage(); 
-    //BMP data
+void repetitive_Task( ) {
+  packet_count ++;
+
+  // Read Sensor Data
+  //GPS data
+  gpsGetTime( &gpsSecond , &gpsMinute, &gpsHour , &gpsDay, &gpsMonth , &gpsYear , &dateValid , &timeValid);
+  gpsReading(&noSats , &lat , &lat , &gpsAltitude , &satsValid, &locValid  , &altValid );
+  //BNO data
+  bnoGetValues();
+  readVoltage();
+  //BMP data
+  if ( currentMode == FLIGHT ) {
     bmpGetValues();
+  }
 
-    // Apply filter
-    
+  // Apply filter
 
-    //Process recieved commmands
-    String CMD_ECHO="";
 
-    //get packet 
-    if( packetAvailable() ){
-      String packetRecieved = getOnePacket();
-      //CMD_ECHO = packetCheck(packetRecieved);
-      packetCheck(packetRecieved);
-    }
+  //Process recieved commmands
+  String CMD_ECHO = "";
 
-    updateAlt(altitude - zero_alt_calib);
-    
-    //Make telemetry packet
-    String telemetry_string = makeTelemetryPacket( );
+  //get packet
+  if ( packetAvailable() ) {
+    String packetRecieved = getOnePacket();
+    packetCheck(packetRecieved);
+  }
 
-    //Transmit data to GCS over Xbee
-    if ( telemetry  ){
-        sendDataTelemetry(telemetry_string);
-    }
-    
-    //Save Data to sd card
-    
-    // Save state to EEPROM
-    EEwriteInt(currentState , 1);
-    EEwriteInt(currentMode, 2);
-    EEwriteInt(packet_count, 3);
+  updateAlt(altitude);
+  //Make telemetry packet
+  String telemetry_string = makeTelemetryPacket( );
+  //Serial.println(telemetry_string);
+  //Transmit data to GCS over Xbee
+  if ( telemetry  ) {
+    sendDataTelemetry(telemetry_string);
+  }
+
+  //Save Data to sd card
+
+  // Save state to EEPROM
+  EEwriteInt(currentState , 1);
+  EEwriteInt(currentMode, 2);
+  EEwriteInt(packet_count, 3);
 }
