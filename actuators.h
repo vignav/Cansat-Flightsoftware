1af60179697069a3aaa75333388460cd61fa9a1f
Vignav <vignav.ram@gmail.com>
2023-05-22 12:27:01 +0530

    pin numbers changeed and all actuators set up

--- actuators.h
+++ actuators.h
@@ -1,53 +1,43 @@
 #include <Servo.h>
+// parachute and probe : servo 
+// landing legs and flag : motor
 
-#define probePin 8
+// onboard transistor : pin 5
+
+#define probePin 28
 #define probeDeployAngle 120
 #define probeLockAngle 50
 
-#define parachutePin 2
+#define parachutePin 29
 #define parachuteDeployAngle 180
 #define parachuteLockAngle 60
 
-#define flagPin 3
-#define flagRaiseAngle 120
-#define flagDownAngle 60
+#define flagPin 7
+#define landinglegsPin 5 
 
 Servo probeServo;
 Servo paraServo;
-Servo flagServo;
+
 void actuatorSetup()
 {
     probeServo.attach(probePin);
     paraServo.attach(parachutePin);
-    flagServo.attach(flagPin);
+    pinMode(flagPin,OUTPUT);
+    pinMode(landinglegsPin,OUTPUT);
 }
 void deployProbe()
 {
     // Turn servo to deploy probe
-   
     probeServo.write(probeDeployAngle);
     return ;
 }
-
 void lockProbe()
 {
     // Turn servo to lock probe
-    
     probeServo.write(probeLockAngle);
     return ;
 }
 
-void deployHeatSheild()
-{
-    // Turn motor ( Turn pin High )
-    return ;
-}
-
-void stopDeployingHeatSheild()
-{
-    // Stop turning motor ( Turn pin Low )
-    return ;
-}
 
 void deployParachute()
 {
@@ -68,15 +58,28 @@ void lockPrachute()
 void raiseFlag()
 {
     // Turn servo to raise flag
-   
-    flagServo.write(flagRaiseAngle);
+    digitalWrite(flagPin,HIGH);
+    return;
+}
+void stopRaisingFlag()
+{
+    // Turn servo to raise flag
+    digitalWrite(flagPin,LOW);
     return;
 }
 
-void lowerFlag()
+
+void deployHeatSheild()
 {
-    // Turn servo lower the flag
-   
-    flagServo.write(flagDownAngle);
-    return;
+    // Turn motor ( Turn pin High )
+    digitalWrite(landinglegsPin,HIGH);
+    return ;
 }
+
+void stopDeployingHeatSheild()
+{
+    // Stop turning motor ( Turn pin Low )
+    digitalWrite(landinglegsPin,LOW);
+    return ;
+}
+
