e7741f7eaf8b15c657a9c64c4b7fe513c874a6ae
Vignav <vignav.ram@gmail.com>
2023-05-30 01:17:56 +0530

    writes eveything to eeprom when reset pin is used

--- smartDelay.h
+++ smartDelay.h
@@ -27,12 +27,20 @@ static void smartDelay(unsigned long ms)
         HS_deployed = false;
         PC_deployed = false;
         MAST_raised = false;
-
-
         zero_alt_calib = 0;
+        
+        EEwriteInt(currentState , 1);
+        EEwriteInt(currentMode, 2);
+        EEwriteInt(packet_count, 3);
+        EEwriteInt(zero_alt_calib, 4);
+        EEwriteInt(HS_deployed, 5);
+        EEwriteInt(PC_deployed, 6);
+        EEwriteInt(MAST_raised, 7);
+        
         telemetry = true;
         tilt_calibration = false ;
         simulation_enabled = false;
+
       }
       if( !SD_works){
         SDsetup();
